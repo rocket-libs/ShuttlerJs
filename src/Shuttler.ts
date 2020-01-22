f0c0e5547b707ae739c8d3c89f390779e8c10b67
nyingi <nyingimaina@gmail.com>
2020-01-22 16:45:20 +0300

    added convenience method 'push' to allow broadcasting changes faster

--- src/Shuttler.ts
+++ src/Shuttler.ts
@@ -1,22 +1,22 @@
 export default class Shuttler<TModel>{
-    model: TModel = {} as TModel;
+    public model: TModel = {} as TModel;
     push: () => void;
 
     constructor(initialModel: TModel){
-      this.writeModel(initialModel);
-      this.push = () => this.writeModel(this.model);
+      this.writeModel(initialModel,true);
+      this.push = () => this.writeModel(this.model,true);
     }
 
     private listeners: {(model: TModel): void; } [] = [];
 
-    subscribe( fn: (model: TModel) => void) : () => void {
+    public subscribe( fn: (model: TModel) => void) : () => void {
         this.listeners.push(fn);
         const unsubscribe = () => this.listeners = this.listeners.filter(singleFn => singleFn !== fn);
         return unsubscribe;
     }
 
-    writeModel(model: TModel){
-        const modelChanged = this.differentObject(this.model,model);
+    writeModel(model: TModel,forceOverwrite: boolean){
+        const modelChanged = forceOverwrite || this.differentObject(this.model,model);
         if(modelChanged){
           this.model = model;
           this.listeners.map(singleFn => singleFn(model));
